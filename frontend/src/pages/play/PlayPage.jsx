4b30168ee042fa49264536d253bfb99bf9b48949
Qian Lu(z5506082) <z5506082@ad.unsw.edu.au>
2025-04-24 07:12:00 +1000

    Implemented the function of displaying question pictures and YouTube videos, and redesigned the style of the answer page

--- frontend/src/pages/play/PlayPage.jsx
+++ frontend/src/pages/play/PlayPage.jsx
@@ -1,22 +1,23 @@
 //=============================================================================
 // File: play/PlayPage.jsx
-// Purpose: Render play page component 
+// Purpose: Render play page component
 // Author: Qian Lu (z5506082@ad.unsw.edu.au)
 // Course: COMP6080
 // Created: 2025-04-23
 //=============================================================================
 
 import { useEffect, useState, useCallback, useRef } from "react";
-import { Spin, Typography, message, Radio, Checkbox, Divider, Table, Card } from "antd";
+import { Typography, message, Radio, Checkbox, Divider, Card } from "antd";
 import { get, put } from "../../utils/request";
+import { useNavigate } from "react-router";
 
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
-export const PlayPage = ({ playerId }) => {
+export const PlayPage = ({ sessionId, playerId }) => {
+  const navigate = useNavigate();
   const [started, setStarted] = useState(false);
-  const [loading, setLoading] = useState(true);
 
-  // 当前题
+  // All questions
   const [question, setQuestion] = useState(null);
   const [lastQuestionId, setLastQuestionId] = useState(null);
   const [correctAnswerIdxs, setCorrectAnswerIdxs] = useState([]);
@@ -25,6 +26,24 @@ export const PlayPage = ({ playerId }) => {
   const timerRef = useRef(null);
   const fetchedAnswerRef = useRef(false);
 
+  const autoWrapStyle = {
+    display: "inline-block",
+    maxWidth: 250,
+    whiteSpace: "nowrap",
+    wordBreak: "break-word",
+    overflow: "hidden",
+  };
+  const showToolTipStyle = {
+    textOverflow: "ellipsis",
+  };
+
+  function getYouTubeId(url) {
+    const m = url.match(
+      /(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([\w-]{11})/
+    );
+    return m ? m[1] : null;
+  }
+
   // Fetch joined status from backend
   const fetchStatus = useCallback(async () => {
     try {
@@ -32,8 +51,6 @@ export const PlayPage = ({ playerId }) => {
       setStarted(!!res.started);
     } catch (err) {
       message.error(err.message);
-    } finally {
-      setLoading(false);
     }
   }, [playerId]);
 
@@ -43,14 +60,16 @@ export const PlayPage = ({ playerId }) => {
       const res = await get(`/play/${playerId}/question`);
       const question = res.question;
       // If not update question, return
-      if (!question || question.id === lastQuestionId) return;
+      if (started && question.id === lastQuestionId) return;
+
       setQuestion(question);
       setLastQuestionId(question.id);
       setSelectedIdxs([]);
 
       // Renew fetch current answer status
       fetchedAnswerRef.current = false;
-      // initial countDown
+
+      // Initial countDown
       clearInterval(timerRef.current);
       const startMs = new Date(question.isoTimeLastQuestionStarted).getTime();
       const endMs = startMs + (question.duration || 10) * 1000;
@@ -97,21 +116,22 @@ export const PlayPage = ({ playerId }) => {
       }
       // End session signal
       if (e.key === `endSessionTime`) {
-        fetchResults();
+        navigate(`/play/${sessionId}/results?playerId=${playerId}`);
       }
     };
     window.addEventListener("storage", onStorage);
     return () => window.removeEventListener("storage", onStorage);
-  }, [fetchQuestion, fetchResults]);
+  }, [fetchQuestion]);
 
   // Automatically pull the current correct answer at the end of the countdown
   useEffect(() => {
     if (countdown === 0 && !fetchedAnswerRef.current) {
       // Record fetch current answer status
-      fetchedAnswerRef.current = true; 
+      fetchedAnswerRef.current = true;
       fetchCorrectAnswer();
+      setTimeout(fetchQuestion, 500);
     }
-  }, [countdown, fetchCorrectAnswer]);
+  }, [countdown, fetchCorrectAnswer, fetchQuestion]);
 
   // Wait started status
   useEffect(() => {
@@ -122,74 +142,169 @@ export const PlayPage = ({ playerId }) => {
     }
   }, [fetchStatus]);
 
-  // Not started or loading
-  if (loading || !started || !question) {
+  if (started && question) {
+    // Answer page
     return (
-      <div style={{
-        height: "100vh", display: "flex",
-        flexDirection: "column", justifyContent: "center", alignItems: "center"
-      }}>
-        <Spin size="large" style={{ marginBottom: 16 }} />
-        <Title level={3}>Please wait</Title>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#fafafa",
+        }}
+      >
+        <Card
+          hoverable
+          style={{
+            width: 300,
+            textAlign: "center",
+            alignContent: "center",
+            backgroundColor: "#e9f8f9",
+          }}
+        >
+          {/* Render video） */}
+          {question.videoUrl &&
+            (() => {
+              const id = getYouTubeId(question.videoUrl);
+              return id ? (
+                <div style={{ marginBottom: 16 }}>
+                  <iframe
+                    width="100%"
+                    height="180"
+                    src={`https://www.youtube.com/embed/${id}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              ) : null;
+            })()}
+          {question.image && (
+            <img
+              alt="Question Image"
+              src={question.image}
+              style={{ backgroundColor: "#d0edf7", height: 180 }}
+            />
+          )}
+          <div>
+            <Text
+              strong
+              style={{
+                ...autoWrapStyle,
+                ...showToolTipStyle,
+                fontSize: 24,
+                color: "#1677ff",
+              }}
+              ellipsis={{ tooltip: question.title }}
+            >
+              {question.title}
+            </Text>
+          </div>
+
+          <Text strong style={{ fontSize: 16, color: "#e05e5e" }}>
+            {" "}
+            Time Limited:{" "}
+          </Text>
+          <Text strong style={{ fontSize: 18, color: "#e05e5e" }}>
+            {countdown != null ? `${countdown} s` : " -- "}
+          </Text>
+          <Divider />
+
+          {question.type === "Single Choice" && (
+            <Radio.Group
+              value={selectedIdxs[0]}
+              onChange={(e) => handleAnswerChange([e.target.value])}
+            >
+              {question.answers.map((opt, idx) => (
+                <div key={idx} style={{ textAlign: "left" }}>
+                  <Text
+                    strong
+                    style={{
+                      ...autoWrapStyle,
+                      ...showToolTipStyle,
+                    }}
+                    ellipsis={{ tooltip: question.title }}
+                  >
+                    <Radio value={idx}>{opt.text}</Radio>
+                  </Text>
+                  <br />
+                </div>
+              ))}
+            </Radio.Group>
+          )}
+
+          {question.type === "Multiple Choice" && (
+            <Checkbox.Group value={selectedIdxs} onChange={handleAnswerChange}>
+              {question.answers.map((opt, idx) => (
+                <Text
+                  key={idx}
+                  strong
+                  style={{
+                    ...autoWrapStyle,
+                    ...showToolTipStyle,
+                    marginBottom: 10,
+                  }}
+                  ellipsis={{ tooltip: question.title }}
+                >
+                  <Checkbox key={idx} value={idx}>
+                    {opt.text}
+                  </Checkbox>
+                </Text>
+              ))}
+            </Checkbox.Group>
+          )}
+
+          {question.type === "Judgement" && (
+            <Radio.Group
+              value={selectedIdxs[0]}
+              onChange={(e) => handleAnswerChange([e.target.value])}
+            >
+              {["True", "False"].map((label, idx) => (
+                <Radio key={idx} value={idx}>
+                  {label}
+                </Radio>
+              ))}
+            </Radio.Group>
+          )}
+
+          {/* ...Render correct answer... */}
+          {countdown === 0 && (
+            <Card
+              size="normal"
+              type="inner"
+              title="Answers"
+              style={{ marginTop: 16, textAlign: "left", color: "#1677ff" }}
+            >
+              <Text>Your Choice:&nbsp;</Text>
+              {selectedIdxs.length === 0 ? (
+                <Text>–</Text>
+              ) : (
+                selectedIdxs.map((i) => {
+                  const isCorrect = correctAnswerIdxs.includes(i);
+                  return (
+                    <Text
+                      strong
+                      key={i}
+                      type={isCorrect ? "success" : "danger"}
+                      style={{ display: "block" }}
+                    >
+                      Option {i + 1}
+                    </Text>
+                  );
+                })
+              )}
+              <br />
+              <Text>Correct Answer:&nbsp;</Text>
+              <br />
+              <Text type="success" strong>
+                {correctAnswerIdxs.length
+                  ? correctAnswerIdxs.map((i) => `Option ${i + 1}`).join(", ")
+                  : "No Answer"}
+              </Text>
+            </Card>
+          )}
+        </Card>
       </div>
     );
   }
-
-  // Answer page
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#fafafa",
-      }}
-    >
-      <Card  style={{ width: 300, textAlign: "center", alignContent:"center"}}>
-        <Title level={2}>Question Title:</Title>
-        <Text>{question.title}</Text>
-        <Text>Time remaining: </Text>
-        <Text strong style={{ fontSize: 18 }}>
-          {countdown != null ? `${countdown} s` : "--"}
-        </Text>
-        <Divider />
-
-        {question.type === "Single Choice" && (
-          <Radio.Group
-            value={selectedIdxs[0]}
-            onChange={e => handleAnswerChange([e.target.value])}
-          >
-            {question.answers.map((opt, idx) => (
-              <Radio key={idx} value={idx} style={{ display: "block", margin: 4 }}>
-                {opt.text}
-              </Radio>
-            ))}
-          </Radio.Group>
-        )}
-
-        {question.type === "Multiple Choice" && (
-          <Checkbox.Group
-            value={selectedIdxs}
-            onChange={handleAnswerChange}
-          >
-            {question.answers.map((opt, idx) => (
-              <Checkbox key={idx} value={idx}>{opt.text}</Checkbox>
-            ))}
-          </Checkbox.Group>
-        )}
-
-        {question.type === "Judgement" && (
-          <Radio.Group
-            value={selectedIdxs[0]}
-            onChange={e => handleAnswerChange([e.target.value])}
-          >
-            {["True","False"].map((label, idx) => (
-              <Radio key={idx} value={idx}>{label}</Radio>
-            ))}
-          </Radio.Group>
-        )}
-      </Card>
-    </div>
-  );
 };
