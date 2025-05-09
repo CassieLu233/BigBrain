b3e9e15ae1638c14c31a508192e6871e63951678
Qian Lu(z5506082) <z5506082@ad.unsw.edu.au>
2025-05-09 19:10:24 +1000

    Continue apply styles on editing question page

--- frontend/src/pages/question/index.jsx
+++ frontend/src/pages/question/index.jsx
@@ -69,6 +69,7 @@ export const QuestionPage = () => {
   // Submit handler
   const handleFinish = async (values) => {
     try {
+      // Build updatedAnswer answers
       let answers;
       if (values.type === "Judgement") {
         answers = [
@@ -152,6 +153,7 @@ export const QuestionPage = () => {
         // Get judgement answer
         let judgementValue = undefined;
         if (currentQuestion.type === "Judgement") {
+          // Ensure answers is effective
           if (currentQuestion.answers.length > 0) {
             const correct = currentQuestion.answers.find(
               (ans) => ans.isCorrect
@@ -171,6 +173,7 @@ export const QuestionPage = () => {
           singleIndex: singleIdx >= 0 ? singleIdx : undefined,
           judgementAnswer: judgementValue,
         });
+        // If image exists, show in upload
         if (currentQuestion.image) {
           setUploadList([
             {
@@ -259,6 +262,135 @@ export const QuestionPage = () => {
           <StyledForm.Item name="videoUrl" label="YouTube URL (Option)">
             <StyledInput placeholder="Optional video link" />
           </StyledForm.Item>
+
+          <StyledForm.Item label="Image (Option)">
+            <StyledUpload
+              listType="picture-card"
+              fileList={uploadList.map((file) => ({
+                uid: file.uid || file.name,
+                name: file.name,
+                status: "done",
+                url: file.url,
+              }))}
+              beforeUpload={beforeUpload}
+              onRemove={handleRemoveThumbnail}
+              showUploadList={{ showPreviewIcon: false, showRemoveIcon: true }}
+            >
+              {uploadList.length === 0 && <PlusOutlined />}
+            </StyledUpload>
+          </StyledForm.Item>
+
+          <StyledDivider>Edit Answers</StyledDivider>
+
+          {questionType === "Judgement" ? (
+            <StyledForm.Item
+              name="judgementAnswer"
+              label="Answer"
+              rules={[
+                { required: true, message: "Please choose True or False" },
+              ]}
+            >
+              <StyledRadioGroup style={{ display: "flex", gap: 16 }}>
+                <StyledRadioGroup.Button value={true}>
+                  True
+                </StyledRadioGroup.Button>
+                <StyledRadioGroup.Button value={false}>
+                  False
+                </StyledRadioGroup.Button>
+              </StyledRadioGroup>
+            </StyledForm.Item>
+          ) : (
+            <StyledForm.List
+              name="answers"
+              rules={[
+                {
+                  validator: async (_, answers) => {
+                    const type = form.getFieldValue("type");
+                    if (type === "Judgement") return Promise.resolve();
+                    if (!answers || !answers.some((a) => a.isCorrect)) {
+                      return Promise.reject(
+                        new Error("Please choose at least one correct answer!")
+                      );
+                    }
+                    return Promise.resolve();
+                  },
+                },
+              ]}
+            >
+              {(fields, { add, remove }, { errors }) => (
+                <>
+                  {fields.map((field, idx) => (
+                    <AnswerItem key={field.key}>
+                      <StyledForm.Item
+                        name={[field.name, "text"]}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Please enter option text",
+                          },
+                        ]}
+                        style={{ flex: 1, margin: 0 }}
+                      >
+                        <StyledInput placeholder={`Option ${idx + 1}`} />
+                      </StyledForm.Item>
+
+                      {questionType === "Single Choice" ? (
+                        <StyledRadioGroup
+                          value={form.getFieldValue("singleIndex")}
+                          onChange={(e) => {
+                            const selectedIndex = e.target.value;
+                            const answers = form.getFieldValue("answers") || [];
+                            const updated = answers.map((ans, i) => ({
+                              ...ans,
+                              isCorrect: i === selectedIndex,
+                            }));
+                            form.setFieldsValue({
+                              answers: updated,
+                              singleIndex: selectedIndex,
+                            });
+                          }}
+                          style={{ margin: "0 16px", flex: "0 0 auto" }}
+                        >
+                          <StyledRadioGroup.Button value={idx}>
+                            Correct
+                          </StyledRadioGroup.Button>
+                        </StyledRadioGroup>
+                      ) : (
+                        <StyledForm.Item
+                          name={[field.name, "isCorrect"]}
+                          valuePropName="checked"
+                          style={{ margin: "0 16px", flex: "0 0 auto" }}
+                        >
+                          <StyledCheckbox>Correct</StyledCheckbox>
+                        </StyledForm.Item>
+                      )}
+
+                      {fields.length > 2 && (
+                        <DangerButton onClick={() => remove(field.name)}>
+                          Remove
+                        </DangerButton>
+                      )}
+                    </AnswerItem>
+                  ))}
+
+                  {fields.length < 6 && (
+                    <StyledForm.Item key="add">
+                      <DashedButton
+                        onClick={() => add({ text: "", isCorrect: false })}
+                        block
+                      >
+                        Add Answer
+                      </DashedButton>
+                    </StyledForm.Item>
+                  )}
+
+                  <ErrorBox>
+                    <StyledForm.ErrorList errors={errors} />
+                  </ErrorBox>
+                </>
+              )}
+            </StyledForm.List>
+          )}
 
           <StyledForm.Item>
             <PrimaryButton htmlType="submit">Save Question</PrimaryButton>
